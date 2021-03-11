42899b499b6e778ca5ac905fd97f478c48aebe36
Uisshu <phngvnc@outlook.com>
2021-03-11 15:45:19 +0100

    get a character als input

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <Arduino.h>
+#include "ascii-table.h"
+
+unsigned char buffer;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.printf("Hello World!\n");
+  if (Serial.available()){
+    buffer = (char) Serial.read();
+
+    if (buffer == NUMBER_1){
+      Serial.printf("Hello 1\n");
+    } else if (buffer == NUMBER_2)
+    {
+      Serial.printf("Hello 2\n");
+    } else
+    {
+      Serial.printf("Error\n");
+    }
+  }
   delay(1000);
 }
